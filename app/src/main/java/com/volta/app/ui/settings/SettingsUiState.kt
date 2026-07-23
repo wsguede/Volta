2221eb3fb600ce239aecba9f179c0136d50a8a3c
wsguede <wsguede@gmail.com>
2026-07-22 20:32:05 -0500

    feat(ui): add appVersion field to SettingsUiState

--- app/src/main/java/com/volta/app/ui/settings/SettingsUiState.kt
+++ app/src/main/java/com/volta/app/ui/settings/SettingsUiState.kt
@@ -2,4 +2,7 @@ package com.volta.app.ui.settings
 
 import com.volta.app.domain.stitching.OutputResolution
 
-data class SettingsUiState(val outputResolution: OutputResolution = OutputResolution.STANDARD)
+data class SettingsUiState(
+    val outputResolution: OutputResolution = OutputResolution.STANDARD,
+    val appVersion: String = ""
+)
